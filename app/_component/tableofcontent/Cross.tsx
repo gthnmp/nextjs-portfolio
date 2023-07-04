e812e78b1abd4527ccd65cbc89ad0f3a6dd89e37
Gathan Mahesa <gthn217@gmail.com>
2023-07-04 20:59:46 +0700

    Update : Added Cross component

--- app/_component/tableofcontent/Cross.tsx
+++ app/_component/tableofcontent/Cross.tsx
@@ -1,7 +1,22 @@
-export default function Cross (){
-  return(
-    <div className="fixed top-1/2 left-20">
-      cross
+import Image, { StaticImageData } from 'next/image'
+import cross from '@/public/cross.svg'
+
+interface CrossProps {
+  icon? : string[] | StaticImageData[]
+}
+
+export default function Cross({ icon = [cross, cross] } : CrossProps){
+  const renderCross = (index : number) => (
+    <div className={`fixed ${index === 0 ? 'left-20 translate-x-1/2' : 'right-20 -translate-x-1/2'} top-1/2`} key={index}>
+      <Image src={icon[index]} alt="Cross graphic element" />
     </div>
+  );
+
+  return(
+    <>
+      {
+        [0,1].map((index) => renderCross(index))
+      }
+    </>
   )
 }
