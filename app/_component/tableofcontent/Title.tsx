8034d7584bf28b3dc1d5ee9e1d910d2d77834ae1
Gathan Mahesa <gthn217@gmail.com>
2023-07-04 14:04:05 +0700

    Update : Created underline animation on TOC

--- app/_component/tableofcontent/Title.tsx
+++ app/_component/tableofcontent/Title.tsx
@@ -8,9 +8,9 @@ type TitleProps = {
 export default function Title ({title, href} : TitleProps){
   const titlePerLines = title.split('\n');
   return(
-    <Link href={href} className="z-40 lg:text-8xl text-5xl flex flex-col justify-center items-center px-8 text-center w-full leading-tight tracking-wide font-serif font-medium uppercase">
+    <Link href={href} className="title z-40 lg:text-8xl text-5xl flex flex-col justify-center items-center px-8 text-center w-full leading-tight tracking-wide font-serif font-medium uppercase ">
       {titlePerLines.map((line,index) => (
-        <span key={index}>{line}</span>
+        <span key={index} className="pb-5">{line}</span>
       ))}
     </Link>
   )
