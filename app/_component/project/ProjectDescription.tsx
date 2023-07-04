30205cc294227885bca8ee690779506dd94cf0a1
Gathan Mahesa <gthn217@gmail.com>
2023-07-04 21:00:38 +0700

    Changed description font style

--- app/_component/project/ProjectDescription.tsx
+++ app/_component/project/ProjectDescription.tsx
@@ -5,7 +5,7 @@ export type DescriptionProps = {
 
 export default function ProjectDescription({ number, paragraph }: DescriptionProps) {
   return (
-    <div className="description w-full px-20  col-start-2">
+    <div className="description w-full px-20  col-start-2 font-light">
       <div className="text-2xl flex gap-10">
         <h2 className="text-gray-400 text-xl">{number}</h2>
         <p className="pl-20 -indent-20 tracking-wide text-justify">{paragraph}</p>
