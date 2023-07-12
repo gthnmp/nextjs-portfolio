9bb9baf640875bca4d24ac8369b8cd5995c25eae
Gathan Mahesa <gthn217@gmail.com>
2023-07-12 20:17:41 +0700

    Update : Added keyword metadata

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,27 +1,28 @@
 import React from 'react';
 import Minimap from '../_component/global/Minimap';
 import Content from '../_component/about/Content';
-import content from '@/app/_api/homepage.json';
+import homepage from '@/app/_api/homepage.json';
 import Wrapper from '@/app/_component/global/SmoothScroll';
 
 export async function generateMetadata() {
-  const { title, description, viewport, robots, charset } = content.metadata;
+  const { title, description, viewport, robots, charset, keywords } = homepage.metadata;
   return {
     title,
     description,
     viewport,
     robots,
-    charset
+    charset,
+    keywords
   };
 }
 
-export default function Homepage(){
-  return (
-    <>
-      <Minimap />
-      <Wrapper className="grid grid-cols-2 lg:grid-cols-3 auto-rows-max lg:px-20 lg:gap-x-20">
-        <Content content={content} />
-      </Wrapper>
-    </>
-  );
-};
+const Homepage = () => (
+  <>
+    <Minimap />
+    <Wrapper className="grid grid-cols-2 lg:grid-cols-3 auto-rows-max lg:px-20 lg:gap-x-20">
+      <Content content={homepage} />
+    </Wrapper>
+  </>
+);
+
+export default Homepage;
